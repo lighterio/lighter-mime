1d47057728c923b985eb7145af61ccdda0e86142
Sam Eubank <sameubank+zerious@gmail.com>
2015-09-17 08:24:05 -0700

    Initial commit

---
+++ test/mkdirp-bench.js
@@ -0,0 +1,16 @@
+var fs = require('fs')
+var mkdirp = require(__dirname + '/lighter-mkdirp')
+
+describe('Benchmarks', function () {
+
+  bench('', function () {
+
+    it('', function () {
+    })
+
+    it('', function () {
+    })
+
+  })
+
+})
---
+++ test/mkdirp-test.js
@@ -0,0 +1,26 @@
+var mkdirp = require('../lighter-mkdirp')
+var is = global.is || require('exam/lib/is')
+
+describe('mkdirp', function () {
+
+  describe('', function () {
+
+    it('', function () {
+    })
+
+    it('', function () {
+    })
+
+  })
+
+  describe('', function () {
+
+    it('', function () {
+    })
+
+    it('', function () {
+    })
+
+  })
+
+})
